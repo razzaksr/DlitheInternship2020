f9398df70435f78b7f261244e97c51636c6d0e95
razzaksr <razzaksr@gmail.com>
2020-08-13 11:05:06 +0530

    jrxml added

--- src/main/java/dlithe/internship/CampusConnect/CampService.java
+++ src/main/java/dlithe/internship/CampusConnect/CampService.java
@@ -1,9 +1,23 @@
 package dlithe.internship.CampusConnect;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
 
 @Service
 public class CampService 
@@ -43,5 +57,35 @@ public class CampService
 	public List<Candidates> fetchViaStatus(String status)
 	{
 		return repo.getByStatus(status);
+	}
+	public String generate(List<Candidates> can,String format)
+	{
+		File fgen=null;
+		try
+		{
+			File file = ResourceUtils.getFile("classpath:CampusReport.jrxml");
+	        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+	        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(can);
+	        Map<String, Object> parameters = new HashMap<>();
+	        parameters.put("createdBy", "Arun Rajpurohit");
+	        parameters.put("createdFor", "DLithe Consultancy Services");
+	        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+	        if (format.equalsIgnoreCase("html")) {
+	        	fgen=new File("dlithe.html");
+	            JasperExportManager.exportReportToHtmlFile(jasperPrint, fgen.getAbsolutePath());
+	        }
+	        if (format.equalsIgnoreCase("pdf")) {
+	        	fgen=new File("dlithe.pdf");
+	            JasperExportManager.exportReportToPdfFile(jasperPrint, fgen.getAbsolutePath());
+	        }
+		}
+		catch(JRException j)
+		{j.printStackTrace();} 
+		catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String hai="Report generated @ "+fgen.getAbsolutePath();
+		return hai;
 	}
 }
